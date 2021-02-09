4c9956757e4b65fb8a0c54d8966349468d9122b9
Kristofer <kristofervool@gmail.com>
2021-02-09 09:36:23 +0200

    Panga front ja back.

--- src/views/Transactionhistory.vue
+++ src/views/Transactionhistory.vue
@@ -20,6 +20,7 @@ let getData = function () {
   this.$http.get('http://localhost:8080/transactionhistory')
       .then(response => this.transactionhistory = response.data)
       .catch(response => console.log(response))
+
 }
 
 export default {
